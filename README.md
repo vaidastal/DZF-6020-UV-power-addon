44dd5f03b4ad27646905c5b0e36ca667ff2b2423
darius <dariuskt@gmail.com>
2024-09-02 05:34:55 +0300

    generating production files for power board

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # DZF-6020-UV-power-addon
 Addon for DZF-6020 vacuum UV post-curing chamber to adjust light sources and their radiant flux.
+
+![PCB 3d main](gen/img_pcb_3d_main_pwr.png)
+
+
+Dev NOTE: before commit, run `./kibot.sh` to regenerate documentation, bom, gerbers and other assets.
+
+* [schematics.pdf](gen/schematics.pdf)
+* [pcb.pdf with dimensions](gen/pcb.pdf)
+* [ibom.html](gen/pwr_single/ibom.html)
+
---
+++ kibot.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# run this script to generate stuff defined in ./project/.kibot.yaml file
+# ./kibot.sh
+
+set -e
+uid=$(id -u)
+gid=$(id -g)
+
+
+function run_kibot() {
+	time docker run --rm -it \
+	--volume "$(pwd):/tmp/workdir" \
+	--workdir "/tmp/workdir" \
+	setsoft/kicad_auto:ki6.0.10_Debian \
+	/bin/bash -c "groupadd -g$gid u; useradd -u$uid -g$gid -d/tmp u; su u -c 'cd project && kibot -c .kibot.yaml $*'"
+}
+
+if [ "$1" ]; then
+	echo "executing kibot with params: $*"
+	run_kibot $*
+	exit 0
+fi
+
+
+# generate documentation stuff
+run_kibot --out-dir ../gen/ --board pwr.kicad_pcb  print_sch pcb_print pcb_img_3d_main full_bom
+#run_kibot --out-dir ../gen/ --board bot.kicad_pcb            pcb_print pcb_img_3d_main
+
+
+# generate single board fab stuff
+run_kibot --skip-pre all --board pwr.kicad_pcb --out-dir ../gen/pwr_single ibom fab_gerbers fab_drill fab_netlist fab_position
+#run_kibot --skip-pre all --board bot.kicad_pcb --out-dir ../gen/bot_single      fab_gerbers fab_drill fab_netlist
+
+
+# concat pcb pdfs
+run_kibot --skip-pre all --out-dir ../gen/ merge_pcb_pdf
+rm ./gen/pcb_*.pdf
+
+# remove garbage changes from pdfs
+sed -i '/[/]CreationDate.*$/d' ./gen/schematics.pdf
+sed -i '/[/]CreationDate.*$/d' ./gen/pcb.pdf
+
+
+# make gerber generation reproducible for git
+sed -i \
+	-e '/^.*TF.CreationDate.*$/d' \
+	-e '/^.*G04 Created by KiCad.* date .*$/d' \
+	-e '/^.*DRILL file .* date .*$/d' \
+	./gen/*/*.{gbr,drl}
+
+
+# move files around
+
+#cp -f ./gen/bom.csv ./gen/pwr_single/_bom.csv
+
+# archive 
+
+function archive() {
+	dir="$(dirname "$1")"
+	rm -f $1
+	touch -cd 1970-01-01T00:00:00Z $dir/*
+	zip -qjorX9 -n zip $1 $dir
+}
+archive ./gen/pwr_single/_prod_pwr.zip
+#archive ./gen/bot_single/_prod_bot.zip
+
+
+
